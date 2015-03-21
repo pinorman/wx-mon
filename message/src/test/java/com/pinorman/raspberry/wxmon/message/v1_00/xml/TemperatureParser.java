b919f09ad7bde6260975919d48fd2ffc35b2c81a
Brian Norman <brian.e.norman@gmail.com>
2015-03-21 15:50:17 -0500

    Starting message module for jaxb xml messages

---
+++ message/src/test/java/com/pinorman/raspberry/wxmon/message/v1_00/xml/TemperatureParser.java
@@ -0,0 +1,90 @@
+package com.pinorman.raspberry.wxmon.message.v1_00.xml;
+
+import java.io.IOException;
+import java.io.StringWriter;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.FileSystemNotFoundException;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.time.LocalDateTime;
+import java.time.Month;
+import java.util.Collections;
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.datatype.DatatypeConfigurationException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TemperatureParser {
+
+    public static void main(String[] args) throws JAXBException, IOException, DatatypeConfigurationException {
+        new TemperatureParser().start();
+    }
+
+    private static final Logger log = LoggerFactory.getLogger(TemperatureParser.class);
+
+    public void start() throws JAXBException, IOException, DatatypeConfigurationException {
+        JAXBContext context = JAXBContext.newInstance(TemperatureReadings.class.getPackage().getName());
+
+        Path path = getResource("temperature-sample.xml");
+        TemperatureReadings temperatureReadings = (TemperatureReadings) context.createUnmarshaller()
+                                                                               .unmarshal(Files.newInputStream(path));
+
+        log.info("Reading temperature from {}", path);
+        for (TemperatureReading reading : temperatureReadings.getValue()) {
+            log.info("    Temperature was {} {} at {}", reading.getTemperature(), reading.getUnit().value(),
+                     reading.getTime());
+        }
+
+        log.info("Writing data read to a string using the JAXB marshaller");
+        StringWriter writer = new StringWriter();
+        context.createMarshaller().marshal(temperatureReadings, writer);
+        log.info("{}", writer);
+
+
+        log.info("Writing data read to a string using the JAXB marshaller");
+        StringWriter anotherWriter = new StringWriter();
+        TemperatureReadings anotherSetOfReadings = new TemperatureReadings();
+        for (int i = 0; i < 5; i++) {
+            TemperatureReading reading = new TemperatureReading();
+            reading.setTemperature(40 + i);
+            reading.setUnit(TemperatureUnit.FAHRENHEIT);
+            reading.setTime(LocalDateTime.of(2015, Month.MARCH, 17, 22, i * 10, 0, 0));
+            anotherSetOfReadings.getValue().add(reading);
+        }
+        context.createMarshaller().marshal(anotherSetOfReadings, anotherWriter);
+        log.info("{}", anotherWriter);
+    }
+
+    public static Path getResource(String stringPath) {
+        try {
+            final ClassLoader loader = Thread.currentThread().getContextClassLoader();
+            if (loader == null) {
+                return null;
+            }
+            final URL resource = loader.getResource(stringPath);
+            if (resource == null) {
+                log.error("Could not find path [{}]", stringPath);
+                return null;
+            }
+
+            final URI uri = resource.toURI();
+            Path path;
+            try {
+                path = Paths.get(uri);
+            } catch (FileSystemNotFoundException ignore) {
+                FileSystems.newFileSystem(uri, Collections.<String, Object>emptyMap());
+                path = Paths.get(uri);
+            }
+            return path;
+        } catch (URISyntaxException | IOException e) {
+            log.error("There was a problem loading path [{}]", stringPath, e);
+        }
+        return null;
+    }
+}
