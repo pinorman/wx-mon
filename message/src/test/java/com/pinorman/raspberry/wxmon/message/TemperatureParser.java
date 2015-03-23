5e56c66901fa88858e016b65071517863e523638
Brian Norman <brian.e.norman@gmail.com>
2015-03-23 18:38:22 -0500

    Reformat of a few files

--- message/src/test/java/com/pinorman/raspberry/wxmon/message/TemperatureParser.java
+++ message/src/test/java/com/pinorman/raspberry/wxmon/message/TemperatureParser.java
@@ -15,7 +15,6 @@ import java.time.Month;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-
 import javax.xml.bind.JAXBException;
 import javax.xml.datatype.DatatypeConfigurationException;
 
@@ -24,63 +23,65 @@ import org.slf4j.LoggerFactory;
 
 public class TemperatureParser {
 
-  public static void main(String[] args) throws Exception {
-    new TemperatureParser().start();
-  }
-
-  private static final Logger log = LoggerFactory.getLogger(TemperatureParser.class);
-
-  public void start() throws JAXBException, IOException, DatatypeConfigurationException, SerializeException {
-    Path path = getResource("temperature-sample.xml");
-    List<TemperatureSensorReading> readings = TemperatureDataSerializer.instance.unmarshal(Files.newInputStream(path));
-
-    log.info("Reading temperature from {}", path);
-    for (TemperatureSensorReading reading : readings) {
-      log.info("    Temperature was {} F at {}", reading.getValue(), reading.getTime());
+    public static void main(String[] args) throws Exception {
+        new TemperatureParser().start();
     }
 
-    log.info("Writing data read to a string using the JAXB marshaller");
-    StringWriter writer = new StringWriter();
-    TemperatureDataSerializer.instance.marshal(readings, writer);
-    log.info("{}", writer);
+    private static final Logger log = LoggerFactory.getLogger(TemperatureParser.class);
+
+    public void start() throws JAXBException, IOException, DatatypeConfigurationException, SerializeException {
+        Path path = getResource("temperature-sample.xml");
+        List<TemperatureSensorReading> readings = TemperatureDataSerializer.instance.unmarshal(
+                Files.newInputStream(path));
+
+        log.info("Reading temperature from {}", path);
+        for (TemperatureSensorReading reading : readings) {
+            log.info("    Temperature was {} F at {}", reading.getValue(), reading.getTime());
+        }
+
+        log.info("Writing data read to a string using the JAXB marshaller");
+        StringWriter writer = new StringWriter();
+        TemperatureDataSerializer.instance.marshal(readings, writer);
+        log.info("{}", writer);
 
 
-    log.info("Writing data read to a string using the JAXB marshaller");
-    StringWriter anotherWriter = new StringWriter();
-    List<TemperatureSensorReading> anotherSetOfReadings = new ArrayList<>();
-    for (int i = 0; i < 5; i++) {
-      TemperatureSensorReading reading =
-        new TemperatureSensorReading(40 + i, LocalDateTime.of(2015, Month.MARCH, 17, 22, i * 10, 0, 0));
-      anotherSetOfReadings.add(reading);
+        log.info("Writing data read to a string using the JAXB marshaller");
+        StringWriter anotherWriter = new StringWriter();
+        List<TemperatureSensorReading> anotherSetOfReadings = new ArrayList<>();
+        for (int i = 0; i < 5; i++) {
+            TemperatureSensorReading reading = new TemperatureSensorReading(40 + i,
+                                                                            LocalDateTime.of(2015, Month.MARCH, 17, 22,
+                                                                                             i * 10, 0, 0));
+            anotherSetOfReadings.add(reading);
+        }
+        TemperatureDataSerializer.instance.marshal(anotherSetOfReadings, anotherWriter);
+        log.info("{}", anotherWriter);
     }
-    TemperatureDataSerializer.instance.marshal(anotherSetOfReadings, anotherWriter);
-    log.info("{}", anotherWriter);
-  }
 
-  public static Path getResource(String stringPath) {
-    try {
-      final ClassLoader loader = Thread.currentThread().getContextClassLoader();
-      if (loader == null) {
+    public static Path getResource(String stringPath) {
+        try {
+            final ClassLoader loader = Thread.currentThread().getContextClassLoader();
+            if (loader == null) {
+                return null;
+            }
+            final URL resource = loader.getResource(stringPath);
+            if (resource == null) {
+                log.error("Could not find path [{}]", stringPath);
+                return null;
+            }
+
+            final URI uri = resource.toURI();
+            Path path;
+            try {
+                path = Paths.get(uri);
+            } catch (FileSystemNotFoundException ignore) {
+                FileSystems.newFileSystem(uri, Collections.<String, Object>emptyMap());
+                path = Paths.get(uri);
+            }
+            return path;
+        } catch (URISyntaxException | IOException e) {
+            log.error("There was a problem loading path [{}]", stringPath, e);
+        }
         return null;
-      }
-      final URL resource = loader.getResource(stringPath);
-      if (resource == null) {
-        log.error("Could not find path [{}]", stringPath);
-        return null;
-      }
-
-      final URI uri = resource.toURI();
-      Path path;
-      try {
-        path = Paths.get(uri);
-      } catch (FileSystemNotFoundException ignore) {
-        FileSystems.newFileSystem(uri, Collections.<String, Object>emptyMap());
-        path = Paths.get(uri);
-      }
-      return path;
-    } catch (URISyntaxException | IOException e) {
-      log.error("There was a problem loading path [{}]", stringPath, e);
     }
-    return null;
-  }
 }
