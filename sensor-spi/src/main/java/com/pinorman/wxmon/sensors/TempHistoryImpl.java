c16a4794d85c1b2b73593201e06f13256667755f
Paul Norman <pinorman412@gmail.com>
2016-02-08 19:17:38 +0000

    moved the temp file i/o into TempHistoryImpl

--- sensor-spi/src/main/java/com/pinorman/wxmon/sensors/TempHistoryImpl.java
+++ sensor-spi/src/main/java/com/pinorman/wxmon/sensors/TempHistoryImpl.java
@@ -1,8 +1,19 @@
 package com.pinorman.wxmon.sensors;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.text.DecimalFormat;
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Deque;
+import java.util.Scanner;
 import java.util.concurrent.ConcurrentLinkedDeque;
+
 
 
 /**
@@ -14,17 +25,50 @@ public class TempHistoryImpl implements TempHistory {
     public static final int MIN_TEMP = -40;
     public static final int MAX_TEMP = 120;
 
+   private static final Logger log = LoggerFactory.getLogger(TempHistoryImpl.class);
+
+    private static DecimalFormat decForm = new DecimalFormat("##0.00");
+    private static DateTimeFormatter dateParser = DateTimeFormatter.ofPattern(" yyyy-MM-d H:m:ss.nnnnnnnnn");       // notice the " " in front of yyyy
+
+
     private Deque<TempReading> qTemp;
+    private String file;
+    private boolean fileWrite;
 
-
-    public TempHistoryImpl() {
-
+    public TempHistoryImpl(String dataFile) {
         qTemp = new ConcurrentLinkedDeque<>();
+        fileWrite = true;
+        double temp;
+        file = dataFile;
+       log.info("Read in any existing temperature data for {} ", dataFile);
+        try (Scanner s = new Scanner(new File(dataFile))) {
+            while (s.hasNext()) {
+                temp = s.nextDouble();      // read in temp from line in file
+                qTemp.add(new TempReading(temp, LocalDateTime.parse(s.nextLine(), dateParser))); // read in date and add in the records we parse from the file
+            }
+        } catch (IOException e) {
+           log.warn("Error reading Temperature file ", e);
+        }
+    }
+    public TempHistoryImpl() {
+        qTemp = new ConcurrentLinkedDeque<>();
+        fileWrite = false;
     }
 
 
-    public void add(TempReading t) {
-        qTemp.add(t);
+    public void add(TempReading temp) {
+        qTemp.add(temp);
+        if(fileWrite) {
+            StringBuilder sb = new StringBuilder();
+            // build string with temp and date;
+            sb.append(decForm.format(temp)).append(dateParser.format(LocalDateTime.now())).append("\n");
+            // write it to the file
+            try (BufferedWriter tOut = new BufferedWriter(new FileWriter(this.file, true))) {
+                tOut.write(sb.toString());
+            } catch (IOException e) {
+                log.warn("Error on Outside Temperature file", e);
+            }
+        }
     }
 
     private boolean queEmpty() {
